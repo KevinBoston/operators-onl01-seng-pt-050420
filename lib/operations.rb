a1b8ccc10eca84bf38e69bbe2c0b40b9882f99dd
Kevin Boston <kbos22@gmail.com>
2020-05-04 15:55:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
 if speed > 60 && speed < 40
-  FALSE
+  true
 else
-  TRUE
+  false
 end
 end
 
 
 def not_safe?(speed)
-	(speed >60 && speed) < 40 ? false : true
+	(speed >60 && speed) < 40 ? true : false
 end
